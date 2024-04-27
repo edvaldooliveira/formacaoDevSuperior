28859eec3def5796ef232dd42aa46ec22ea18a1b
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-04-27 11:02:55 -0300

    Estrutura do projeto

---
+++ desafiocomponentesinjecaodependencia/src/main/java/com/componentesinjecaodependencia/desafiocomponentesinjecaodependencia/DesafiocomponentesinjecaodependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocomponentesinjecaodependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocomponentesinjecaodependenciaApplication.class, args);
+	}
+
+}
---
+++ desafiocomponentesinjecaodependencia/src/test/java/com/componentesinjecaodependencia/desafiocomponentesinjecaodependencia/DesafiocomponentesinjecaodependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocomponentesinjecaodependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
