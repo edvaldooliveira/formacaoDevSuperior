aefe3316166a1cde200b24ccfb166612b0eedfa8
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-04-27 22:39:16 -0300

    Implementaçção da clase ShippingService

--- desafiocomponentesinjecaodependencia/src/main/java/com/componentesinjecaodependencia/desafiocomponentesinjecaodependencia/DesafiocomponentesinjecaodependenciaApplication.java
+++ desafiocomponentesinjecaodependencia/src/main/java/com/componentesinjecaodependencia/desafiocomponentesinjecaodependencia/DesafiocomponentesinjecaodependenciaApplication.java
@@ -1,13 +1,32 @@
 package com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia;
 
+
+import com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia.entities.Order;
+import com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia.services.ShippingService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafiocomponentesinjecaodependenciaApplication {
+public class DesafiocomponentesinjecaodependenciaApplication  implements CommandLineRunner {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafiocomponentesinjecaodependenciaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Order order = new Order(1034, 0D, 20D);
+		ShippingService shippingService = new ShippingService();
+
+		System.out.println("Resultado = " + shippingService.shipment(order));
+
+
+
+
+	}
 }
---
+++ desafiocomponentesinjecaodependencia/src/main/java/com/componentesinjecaodependencia/desafiocomponentesinjecaodependencia/services/OrderService.java
@@ -0,0 +1,19 @@
+package com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia.services;
+
+import com.componentesinjecaodependencia.desafiocomponentesinjecaodependencia.entities.Order;
+
+//Responsavel de operações relacionado a pedito
+//Serviço de pedido
+public class OrderService {
+
+
+    //Responsavel or operações relacionado a pedito,  Exemplo Valor total: R$ 720.00
+    public double total(Order order){
+
+
+
+        return 10;
+    }
+
+
+}
