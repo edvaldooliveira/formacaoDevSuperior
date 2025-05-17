dc436b81139a9bfaeaeb460482a81941c909ce98
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-17 14:25:32 +0100

    geteres e seters implementado

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/entities/Client.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/entities/Client.java
@@ -32,5 +32,51 @@ public class Client {
         this.children = children;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
 }
