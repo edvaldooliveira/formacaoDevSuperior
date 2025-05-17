7fa691e572603b7a0fdee6369fddfb1377d1a75d
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-17 14:04:18 +0100

    Implementado as anotações do spring

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/entities/Client.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/entities/Client.java
@@ -1,14 +1,21 @@
 package com.desafioapirestcrud.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
     private Double income;
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     private Integer children;
