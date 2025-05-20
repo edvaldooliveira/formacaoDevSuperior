fb0bbf09aca76509827052d0fcfe17072350b894
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-20 17:35:44 +0100

    Início DTO

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
@@ -1,5 +1,18 @@
 package com.desafioapirestcrud.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cliente")
 public class ClientController {
+
+
+    @GetMapping
+    public String teste() {
+        return "ClienteTransfor";
+    }
 
 }
---
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/dto/ClientDTO.java
@@ -0,0 +1,10 @@
+package com.desafioapirestcrud.dto;
+
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+
+}
