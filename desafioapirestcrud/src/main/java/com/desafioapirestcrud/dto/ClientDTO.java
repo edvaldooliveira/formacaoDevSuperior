c80079d75d9228fbff1161d7328fa567e9067afe
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-23 15:50:29 +0100

    Cliente DTO

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/dto/ClientDTO.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/dto/ClientDTO.java
@@ -1,5 +1,7 @@
 package com.desafioapirestcrud.dto;
 
+import com.desafioapirestcrud.entities.Client;
+
 public class ClientDTO {
 
     private Long id;
@@ -7,4 +9,34 @@ public class ClientDTO {
     private String cpf;
     private Double income;
 
+      public ClientDTO(Long id, String name, String cpf, Double income) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+    }
+
+
+    public ClientDTO(Client entity) {
+        id = entity.getId();
+        name = entity.getName();
+        cpf = entity.getCpf();
+        income = entity.getIncome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
 }
