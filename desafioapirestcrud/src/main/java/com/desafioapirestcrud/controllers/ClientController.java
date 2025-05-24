42e0aa5af34d07c53fb575c4cac84024c0740bfe
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-24 15:21:49 +0100

    ClientRepository

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
@@ -1,18 +1,30 @@
 package com.desafioapirestcrud.controllers;
 
 
+import com.desafioapirestcrud.clientRepository.ClientRepository;
+import com.desafioapirestcrud.entities.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/cliente")
 public class ClientController {
 
+    @Autowired
+    ClientRepository repository;
 
     @GetMapping
     public String teste() {
-        return "ClienteTransfor";
+
+     Optional<Client> result = repository.findById(1L);
+
+        Client client = result.get();
+       return client.getName();
+
     }
 
 }
--- desafioapirestcrud/src/main/resources/import.sql
+++ desafioapirestcrud/src/main/resources/import.sql
@@ -1,1 +1,1 @@
-
+INSERT INTO tb_client ( name, cpf, income, birth_date, children) VALUES ('João Silva', '12345678901', 3500.00, '1990-05-20', 2);
