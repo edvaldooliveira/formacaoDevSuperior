10abf054f08f15ad08a34bfc2265abca016bcd57
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-05-27 16:51:08 +0100

    busca paginada

--- desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
+++ desafioapirestcrud/src/main/java/com/desafioapirestcrud/controllers/ClientController.java
@@ -5,17 +5,16 @@ package com.desafioapirestcrud.controllers;
 import com.desafioapirestcrud.dto.ClientDTO;
 
 import com.desafioapirestcrud.services.ClientService;
-import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
-import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping(value = "/cliente")
@@ -32,8 +31,8 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<ClientDTO> findAll() {
-       return service.findAll();
+    public Page<ClientDTO> findAll(Pageable pageable) {
+       return service.findAll(pageable);
     }
 
 
